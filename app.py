12111091257ca8ad509e0c7fe1d9fd75f249484a
Fellipe Leonardo <112525075+rakRandom@users.noreply.github.com>
2024-07-25 03:55:36 -0300

    feat: detach download page from the others, add icon and a base html

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, send_from_directory, abort, render_template
 
 app = Flask(__name__)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Defines the directory where the files will be
 FILE_DIRECTORY = app.root_path + "\\download"
@@ -12,7 +12,12 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/<filename>')
+@app.route("/download")
+def download():
+    return render_template("download.html")
+
+
+@app.route("/download/<filename>")
 def download_file(filename):
     try:
         return send_from_directory(FILE_DIRECTORY, filename, as_attachment=True)
@@ -20,5 +25,5 @@ def download_file(filename):
         abort(404)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=2024)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=2024)
