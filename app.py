dd15e9a126fe28324897b98ef141e0118517ee80
Fellipe Leonardo <112525075+rakRandom@users.noreply.github.com>
2024-07-26 06:05:08 -0300

    feat: add find_host page

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
-from flask import Flask, send_from_directory, abort, render_template  # , session
+from flask import Flask, send_from_directory, abort, render_template, request, session
 from flask_socketio import SocketIO
 from host_finder import HostFinder
-# from flask_session import Session
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
-# app.config["SESSION_PERMANENT"] = False
-# app.config["SESSION_TYPE"] = "filesystem"
-# Session(app)
 
 socketio = SocketIO(app)
 hostf = HostFinder()
@@ -16,21 +12,22 @@ hostf = HostFinder()
 FILE_DIRECTORY = app.root_path + "\\download"
 
 
-@socketio.on('start_scan')
-def handle_start_scan(data):
-    scan_ports = data.get('ports', [80, 8000, 8080])
-    
+@socketio.on('find_hosts_process')
+def find_hosts_process():
     def background_task(scan_ports):
         results = hostf.scan_network(scan_ports)
-        socketio.emit('scan_results', results)
+        socketio.emit('find_hosts_process', results)
     
-    socketio.start_background_task(target=background_task, scan_ports=scan_ports)
+    socketio.start_background_task(target=background_task, scan_ports=[2024])
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/find_hosts")
+def find_hosts():
+    return render_template("find_host.html")
 
 @app.route("/download")
 def download():
@@ -46,4 +43,4 @@ def download_file(filename):
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=2024)
