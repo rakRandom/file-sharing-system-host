753fb7f78502579de200151d67354395c8c7aaaf
Fellipe Leonardo <112525075+rakRandom@users.noreply.github.com>
2024-07-27 19:46:33 -0300

    feat: change support to fss client instead of host

--- app.py
+++ app.py
@@ -16,9 +16,6 @@ socketio = SocketIO(app)
 # Create HostFinder object to find hosts for client
 hostf = HostFinder()
 
-# Defines the directory where the files will be
-FILE_DIRECTORY = app.root_path + "\\download"
-
 # ==============================================================================================
 
 # WebSocket function
@@ -31,13 +28,8 @@ def find_hosts():
     socketio.start_background_task(target=background_task, scan_ports=[2024])
 
 
-PAGES = {
-    "host_finder": "host_finder.html",
-    "download": "download.html",
-    "files": "files.html"
-}
-
 # ========================= Pages =========================
+
 @app.route("/")
 @app.route("/<page>")
 def render_page(page="index"):
@@ -45,47 +37,22 @@ def render_page(page="index"):
         return render_template("index.html", host=request.args.get('h'))
     return render_template("not_found.html")
 
+
 @app.route("/host_finder")
 def host_finder():
     return render_template("host_finder.html")
+
 
 @app.route("/download")
 def download():
     return render_template("download.html", name=request.args.get("name"))
 
+
 @app.route("/files")
 def files():
     return render_template("files.html")
 
-
 # ===============================================
-TOTALLY_SECRET_PASSWORD = "1234"
-
-# Todo: Client version with this endpoint (Delete this before release)
-@app.route("/verify")
-def verify():
-    if request.args.get('p') != TOTALLY_SECRET_PASSWORD:
-        return jsonify({"response": False}), 200
-    
-    return jsonify({"response": True}), 200
-
-# Todo: Client version with this endpoint (Delete this before release)
-@app.route("/list_files2")
-def list_files2():
-    if request.args.get('p') != TOTALLY_SECRET_PASSWORD:
-        return jsonify({"response": []}), 200
-
-    file_list: list[str] = []
-
-    for dirpath, _, filenames in os.walk("."):
-        for filename in filenames:
-            if dirpath.startswith(".\\download"):
-                file_list.append(filename)
-    
-    return jsonify({"response": file_list}), 200
-
-# ===============================================
-
 
 # Connect user
 @app.route("/connect")
@@ -120,7 +87,7 @@ def disconnect():
 @app.route("/list_files")
 def list_files():
     conn_code = session.get("connection-code")
-    r = requests.get(f"http://{conn_code[0]}/list_files2?p={conn_code[1]}")
+    r = requests.get(f"http://{conn_code[0]}/list_files?p={conn_code[1]}")
 
     if r.status_code != 200:
         return jsonify({"response": []}), 400
@@ -129,16 +96,13 @@ def list_files():
 
     return jsonify({"response": res["response"]}), 200
 
-# Request a download
-@app.route("/download/<filename>")
-def download_file(filename):
-    try:
-        return send_from_directory(FILE_DIRECTORY, filename, as_attachment=True)
-    except FileNotFoundError:
-        abort(404)
+
+@app.route("/get_host")
+def get_host():
+    return jsonify({"response": session.get("connection-code")})
 
 # ==============================================================================================
 
 # Run the app
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=2024)
+    app.run(host="0.0.0.0", port=8080)
--- static/js/download.js
+++ static/js/download.js
@@ -9,10 +9,17 @@ button.addEventListener("click", e => {
     button.innerText = "Download File";
 });
 
-async function downloadFile(path) {
+async function downloadFile(name) {
+    const credentials = await fetch("/get_host").then(res => res.json()).then(json => json.response);
+    console.log(credentials);
+    if (!(Array.isArray(credentials) && credentials.length)) {
+        alert("Error: fail to get host credentials")
+        return;
+    }
+
     let link = document.createElement("a");
     link.style.display = "hidden";
-    link.href = "/download/" + path;
+    link.href = `http://${credentials[0]}/download/${name}?p=${credentials[1]}`;
     link.download = "";
 
     document.body.appendChild(link);
